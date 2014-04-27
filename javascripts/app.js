a3c11d543dabdcfcab4f4ef7affbaf2af2cf6b76
peti193 <iszambsc@gmail.com>
2014-04-27 10:55:06 +0200

    some file added

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,94 +1,22 @@
-jQuery(document).ready(function ($) {
-
-	/* Use this js doc for all application specific JS */
-
-	/* TABS --------------------------------- */
-	/* Remove if you don't need :) */
-
-	function activateTab($tab) {
-		var $activeTab = $tab.closest('dl').find('a.active'),
-				contentLocation = $tab.attr("href") + 'Tab';
-				
-		// Strip off the current url that IE adds
-		contentLocation = contentLocation.replace(/^.+#/, '#');
-
-		//Make Tab Active
-		$activeTab.removeClass('active');
-		$tab.addClass('active');
-
-    //Show Tab Content
-		$(contentLocation).closest('.tabs-content').children('li').hide();
-		$(contentLocation).css('display', 'block');
+var bird = function() {
+	this.fly = function() {
+		console.log("I'm fly on my way");
 	}
 
-  $('dl.tabs dd a').live('click', function (event) {
-    activateTab($(this));
-  });
+	this.pen = false;
+};
 
-	if (window.location.hash) {
-		activateTab($('a[href="' + window.location.hash + '"]'));
-		$.foundation.customForms.appendCustomMarkup();
-	}
-
-	/* ALERT BOXES ------------ */
-	$(".alert-box").delegate("a.close", "click", function(event) {
-    event.preventDefault();
-	  $(this).closest(".alert-box").fadeOut(function(event){
-	    $(this).remove();
-	  });
-	});
+chicken = new bird();
 
 
-	/* PLACEHOLDER FOR FORMS ------------- */
-	/* Remove this and jquery.placeholder.min.js if you don't need :) */
+bird.prototype.eat = function(something , number) {
+	console.log("eat this:" + something + number);
+};
 
-	$('input, textarea').placeholder();
-
-	/* TOOLTIPS ------------ */
-	$(this).tooltips();
+chicken.fly();
+console.log(chicken.pen);
+chicken.eat("seeds" , 20);
 
 
 
-	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
-//	$('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-//	$('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-//	$('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-//	$('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
 
-
-
-	/* DROPDOWN NAV ------------- */
-
-	var lockNavBar = false;
-	$('.nav-bar a.flyout-toggle').live('click', function(e) {
-		e.preventDefault();
-		var flyout = $(this).siblings('.flyout');
-		if (lockNavBar === false) {
-			$('.nav-bar .flyout').not(flyout).slideUp(500);
-			flyout.slideToggle(500, function(){
-				lockNavBar = false;
-			});
-		}
-		lockNavBar = true;
-	});
-  if (Modernizr.touch) {
-    $('.nav-bar>li.has-flyout>a.main').css({
-      'padding-right' : '75px'
-    });
-    $('.nav-bar>li.has-flyout>a.flyout-toggle').css({
-      'border-left' : '1px dashed #eee'
-    });
-  } else {
-    $('.nav-bar>li.has-flyout').hover(function() {
-      $(this).children('.flyout').show();
-    }, function() {
-      $(this).children('.flyout').hide();
-    })
-  }
-
-
-	/* DISABLED BUTTONS ------------- */
-	/* Gives elements with a class of 'disabled' a return: false; */
-	$('.placeholder').placeholder();
-  
-});
